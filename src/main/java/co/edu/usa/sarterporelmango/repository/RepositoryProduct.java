3d0f44a77e572962797e69ae54d3c9b9c4e8f65b
Jorge Prieto <jprietof@outlook.es>
2021-12-06 06:47:06 -0500

    se crearon los repository de user y product

--- src/main/java/co/edu/usa/sarterporelmango/repository/RepositoryProduct.java
+++ src/main/java/co/edu/usa/sarterporelmango/repository/RepositoryProduct.java
@@ -1,5 +1,39 @@
 package co.edu.usa.sarterporelmango.repository;
 
-public class RepositoryProduct {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import co.edu.usa.sarterporelmango.model.Product;
+import co.edu.usa.sarterporelmango.model.dao.IProduct;
+
+import java.util.*;
+/**
+ * 
+ * @author jprietof
+ *
+ */
+@Repository
+public class RepositoryProduct {
+	@Autowired
+	private IProduct productCrudRepository;
+	
+	public List<Product> getAll(){
+		return productCrudRepository.findAll();
+	}
+	
+	public Optional<Product> getProduct(String reference){
+		return productCrudRepository.findById(reference);
+	}
+	
+	public Product create(Product product){
+		return productCrudRepository.save(product);
+	}
+	
+	public void update(Product product){
+		productCrudRepository.save(product);
+	}
+	
+	public void delete(Product product){
+		productCrudRepository.delete(product);
+	}
 }
