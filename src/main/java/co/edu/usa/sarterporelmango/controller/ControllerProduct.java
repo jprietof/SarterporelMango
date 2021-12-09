d5e84044c2787cddb33cd5e3224fa916f8989be9
Jorge Prieto <jprietof@outlook.es>
2021-12-08 23:15:44 -0500

    service y controler, de user y product

--- src/main/java/co/edu/usa/sarterporelmango/controller/ControllerProduct.java
+++ src/main/java/co/edu/usa/sarterporelmango/controller/ControllerProduct.java
@@ -1,5 +1,59 @@
 package co.edu.usa.sarterporelmango.controller;
 
-public class ControllerProduct {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
+import co.edu.usa.sarterporelmango.model.Product;
+import co.edu.usa.sarterporelmango.service.ServiceProduct;
+
+/**
+ * 
+ * @author jprietof
+ *
+ */
+@RestController
+@RequestMapping("/api/cookware")
+@CrossOrigin("*")
+public class ControllerProduct {
+	@Autowired
+	private ServiceProduct accesoryService;
+	
+	@GetMapping("/all")
+	public List<Product> getAll(){
+		return accesoryService.getAll();
+	}
+	
+	@PostMapping("/new")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Product create(@RequestBody Product gadget) {
+		return accesoryService.create(gadget);
+	}
+	
+	@PutMapping("/update")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Product update(@RequestBody Product gadget) {
+		return accesoryService.update(gadget);
+	}
+	
+	@GetMapping("/{reference}")
+	public Optional<Product> getCook(@PathVariable("reference") String reference){
+		return accesoryService.getCook(reference);
+	}
+	
+	@DeleteMapping("{reference}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public boolean delete(@PathVariable("reference") String reference) {
+		return accesoryService.delete(reference);
+	}
 }
--- src/main/java/co/edu/usa/sarterporelmango/service/ServiceProduct.java
+++ src/main/java/co/edu/usa/sarterporelmango/service/ServiceProduct.java
@@ -1,5 +1,82 @@
 package co.edu.usa.sarterporelmango.service;
 
-public class ServiceProduct {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import co.edu.usa.sarterporelmango.model.Product;
+import co.edu.usa.sarterporelmango.repository.RepositoryProduct;
+import java.util.*;
+
+/**
+ * 
+ * @author jprietof
+ *
+ */
+@Service
+public class ServiceProduct {
+	@Autowired
+	private RepositoryProduct cookRepository;
+	
+	public List<Product> getAll(){
+		return cookRepository.getAll();
+	}
+	
+	public Optional<Product> getCook(String reference){
+		return cookRepository.getProduct(reference);
+	}
+	
+	public Product create(Product accesory) {
+		if(accesory.getReference()==null) {
+			return accesory;
+		}else{
+			return cookRepository.create(accesory);
+		}
+	}
+	
+	public Product update(Product accesory) {
+		if(accesory.getReference() !=null) {
+			Optional<Product> accesoryDb = cookRepository.getProduct(accesory.getReference());
+			if(!accesoryDb.isEmpty()) {
+				if(accesory.getBrand() !=null) {
+					accesoryDb.get().setBrand(accesory.getBrand());
+				}
+				if(accesory.getCategory() !=null) {
+					accesoryDb.get().setCategory(accesory.getCategory());
+				}
+				if(accesory.getDescription() !=null) {
+					accesoryDb.get().setDescription(accesory.getDescription());;
+				}
+				if(accesory.getDimensiones() !=null) {
+					accesoryDb.get().setDimensiones(accesory.getDimensiones());
+				}
+				if(accesory.getMateriales() !=null) {
+					accesoryDb.get().setMateriales(accesory.getMateriales());
+				}
+				if(accesory.getPrice() !=0.0) {
+					accesoryDb.get().setPrice(accesory.getPrice());
+				}
+				if(accesory.getQuantity() !=0) {
+					accesoryDb.get().setQuantity(accesory.getQuantity());
+				}
+				if(accesory.getPhotography() !=null) {
+					accesoryDb.get().setPhotography(accesory.getPhotography());
+				}
+				accesoryDb.get().setAvailability(accesory.getAvailability());
+				cookRepository.update(accesoryDb.get());
+				return accesoryDb.get();
+			}else {
+				return accesory;
+			}
+		}else {
+			return accesory;
+		}
+	}
+	
+	public boolean delete(String reference) {
+		Boolean aBoolean = getCook(reference).map(accesory->{
+			cookRepository.delete(accesory);
+			return true;
+		}).orElse(false);
+		return aBoolean;
+	}
 }
